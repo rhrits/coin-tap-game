9b92596e5bf351ac66b6eea6df3f0be25f9210c6
rhrits <hritik.hritiksingh@gmail.com>
2024-10-23 15:46:30 +0530

    up

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import CoinTapGame from './components/CoinTapGame'; // Adjust the path if needed
 const App: React.FC = () => {
   return (
     <div style={{ textAlign: 'center', padding: '20px' }}>
-      <h1 style={{ color: 'white' }}>Play This Game</h1>
+      <h1 style={{ color: 'white' }}>Play Game</h1>
       <CoinTapGame />
     </div>
   );
